d074d897d3eb483a254cf8247d8b1e1a7ae06013
Piero Delgado <112528610+PieroD04@users.noreply.github.com>
2025-06-11 12:48:11 -0500

    chore: remove unnecesary post

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ const mqttService = new MqttService();
 // Express Setup
 const app = express();
 app.use(cors());
-app.use(express.json());
 
 app.get("/data", (req: Request, res: Response) => {
   const latestData: SensorData | null = mqttService.getLatestData();
@@ -17,13 +16,6 @@ app.get("/data", (req: Request, res: Response) => {
   } else {
     res.status(404).json({ error: "No data received yet" });
   }
-});
-
-app.post("/data", (req: Request, res: Response) => {
-  const data: SensorData = req.body;
-  console.log("Datos recibidos del ESP32:", data);
-  mqttService["latestData"] = { ...data, timestamp: Date.now() };
-  res.status(200).send("OK");
 });
 
 const PORT = process.env.PORT || 3000;
